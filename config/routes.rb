d74d984b30601c33958c9da71a9a274de6a931cb
EC2 Default User <ec2-user@ip-172-31-2-105.ap-northeast-1.compute.internal>
2022-10-06 07:18:53 +0000

    [Update]Rails App1-10

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  post 'lists' => 'lists#create' #追記した
+  # get 'lists/new'
+  # post 'lists' => 'lists#create'
   # get 'lists/index' ↓に書き換えた
-  get 'lists' => 'lists#index'
+  # get 'lists' => 'lists#index'
   # get 'lists/show' ↓に書き換えた
-  get 'lists/:id' => 'lists#show'
-  get 'lists/edit'
+  # get 'lists/:id' => 'lists#show', as: 'list'
+  # get 'lists/edit' ↓に書き換えた
+  # get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  # patch 'list/:id' => 'lists#update' , as: 'update_list'
+  # delete 'list/:id' => 'lists#destroy', as: 'destroy_list'
   # get 'homes/top' ↓に書き換えた
   get '/top' => 'homes#top'
+  resources :lists
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
